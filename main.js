9070abfd78ed2b969dd7a7018aaadab64e2afbaf
Kevin Frost <kevin@TL.thrivelife.com>
2020-07-25 18:58:22 -0600

    fixing some code up

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ first(people).map(person => {
     let li = createNode('li'),
         span = createNode('span');
 
-    span.innerText = `${person.name} ${person.gender}`;
+    span.innerText = `${person.name}`;
 
     append(li, span);
     append(peopleul, li);
@@ -32,7 +32,7 @@ first(planets).map(planet => {
         let li = createNode('li'),
             span = createNode('span');
     
-        span.innerText = `${planet.name} ${planet.diameter}`;
+        span.innerText = `${planet.name}`;
     
         append(li, span);
         append(planetsul, li);
@@ -66,52 +66,3 @@ fetch(url)
             img = createNode('img'),
             span = createNode('span');
     })
-
-
-
-
-
-//Original Code
-/*import { people } from './people.js'
-import { planets } from './planets.js'
-
-people.map(people =>{
-    console.log(people.name);
-})
-
-function createNode(element) {
-    return document.createElement(element);
-}
-
-function append(parent, el) {
-    return parent.appendChild(el);
-}
-
-//const swurl = "https://swapi.dev/api/people/"
-
-//baseball Assignment Data
-const url = './people.js';
-const ul = document.getElementById('people');
-
-let swpeople;
-
-
-fetch(url)
-    .then(resp => resp.json()
-    .then(data => {
-        swpeople = data.results;
-
-        swpeople.map(person =>{
-            console.log(people.name)
-            let li = createNode('li'),
-                span = createNode('span');
-
-            span.innerHTML = `${people.name}`;
-
-            append(li, img);
-            append(li, span);
-            append(ul, li);
-        })
-    })
-)
-*/
